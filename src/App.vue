88c1e80aed6d1209a3258eb0a62c94d21548b6b0
WanHasiev <hasif.halim00@gmail.com>
2022-11-03 13:41:16 +0800

    test commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <!-- <HelloWorld></HelloWorld> -->
+  <CalculatorApp></CalculatorApp>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import CalculatorApp from './components/CalculatorApp.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    CalculatorApp
+}
+}
+</script>
+
+<style>
+  body {
+    background: #31475e !important;
+  }
+</style>
